360d2e214c9c1d5a6fdfbaafe06cad5dd7d186b0
eclipsemode <eclipsemod555@gmail.com>
2024-09-23 19:12:07 +0300

    forgot merge

--- cmd/migrator/migrations/000002_add-locations-table.up.sql
+++ cmd/migrator/migrations/000002_add-locations-table.up.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS locations (
-    "id" SERIAL PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "city" VARCHAR(255) NOT NULL,
     "federal_district" VARCHAR(255),
     "fias_id" VARCHAR(255),

ba8c9f44538d297ac3824b6b5f6da5ba94b5bf45
eclipsemode <eclipsemod555@gmail.com>
2024-09-29 20:58:48 +0300

    migrations add

---
+++ cmd/migrator/migrations/000007_add-stays-reviews-s-table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS stays_reviews
+(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    stay_id UUID NOT NULL REFERENCES stays(id),
+    user_id UUID NOT NULL REFERENCES users(id),
+    title VARCHAR(255) NOT NULL,
+    description TEXT DEFAULT '',
+    rating FLOAT DEFAULT 0.0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);

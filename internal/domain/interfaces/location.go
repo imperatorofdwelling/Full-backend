10c3223848980adca9c72b9774e1005cb4438625
Daniel <eclipsemod-post@yandex.ru>
2024-11-15 18:52:13 +0300

    create interfaces

--- internal/domain/interfaces/location.go
+++ internal/domain/interfaces/location.go
@@ -1,7 +1,5 @@
 package interfaces
 
-
-
 import (
 	"context"
 	"github.com/gofrs/uuid"
@@ -13,14 +11,26 @@ import (
 type LocationRepo interface {
 	FindByNameMatch(ctx context.Context, match string) (*[]models.Location, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*models.Location, error)
+	GetAll(ctx context.Context) (*[]models.Location, error)
+	GetOneByID(ctx context.Context, id uuid.UUID) (*models.Location, error)
+	DeleteByID(ctx context.Context, id uuid.UUID) error
+	UpdateByID(ctx context.Context, id uuid.UUID, location models.LocationEntity) error
 }
 
 //go:generate mockery --name LocationService
 type LocationService interface {
 	FindByNameMatch(ctx context.Context, match string) (*[]models.Location, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*models.Location, error)
+	GetAll(ctx context.Context) (*[]models.Location, error)
+	GetOneByID(ctx context.Context, id uuid.UUID) (*models.Location, error)
+	DeleteByID(ctx context.Context, id uuid.UUID) error
+	UpdateByID(ctx context.Context, id uuid.UUID, location models.LocationEntity) error
 }
 
 type LocationHandler interface {
 	FindByNameMatch(http.ResponseWriter, *http.Request)
+	GetAll(http.ResponseWriter, *http.Request)
+	GetOneByID(http.ResponseWriter, *http.Request)
+	DeleteByID(http.ResponseWriter, *http.Request)
+	UpdateByID(http.ResponseWriter, *http.Request)
 }
